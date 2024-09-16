f5b602935d74c164c7a7319937e1873692e8a285
majosetax <mariajolopezsc@gmail.com>
2024-09-16 14:46:01 -0600

    cambios migraciones

--- database/migrations/2024_09_09_214012_create_elementos_fisicos_table.php
+++ database/migrations/2024_09_09_214012_create_elementos_fisicos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('elementos_fisicos', function (Blueprint $table) {
             $table->id();
-            $table->string('codigo');
+            $table->string('codigo')->unique();
             $table->string('marca');
             $table->string('modelo');
             $table->string('ubicacion_interna');
